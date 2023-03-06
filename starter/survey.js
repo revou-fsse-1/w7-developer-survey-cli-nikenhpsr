354568aa4382ec26d9da35caa084c02048d3a373
nikenhpsr <niken18003@mail.unpad.ac.id>
2023-03-06 21:40:20 +0700

    fix error

--- starter/survey.js
+++ starter/survey.js
@@ -1,29 +1,7 @@
-import inquirer from "inquirer";
-/*
-const questions = [
-  "What's your first name?",
-  "Hello %s What's your email address?",
-  "Are you experienced Developer?",
-  "How many years of experience you have with JavaScript?",
-  "What JavaScript library do you know?",
-  "What is your desired salary?"
-];
-
-// run your command
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    console.log(JSON.stringify(answers, null, 2));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log("Your console environment is not supported!");
-    } else {
-      console.log(error);
-    }
-  });
-*/
-  var inquirer = require('inquirer');
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+import inquirer from 'inquirer';
+var inquirer = require('inquirer');
 
 const questions = [
     {
